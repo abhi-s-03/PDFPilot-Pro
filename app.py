d8790aafa07efc97e0ac45d97c9bd0d7b1f31588
abhi-s-03 <abhisheks3002@gmail.com>
2024-01-26 21:48:47 +0530

    Refactor imports and fix formatting in app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from PyPDF2 import PdfReader
-import google.generativeai as genai
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
 from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
@@ -13,12 +14,12 @@ load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def pdf_text(docs):
-    text = ""
+    text=""
     for pdf in docs:
-        pdf_reader = PdfReader(pdf)
+        pdf_reader= PdfReader(pdf)
         for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
+            text+= page.extract_text()
+    return  text
 
 def text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
@@ -26,7 +27,7 @@ def text_chunks(text):
     return chunks
 
 def vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
 
@@ -39,25 +40,19 @@ def get_conversational_chain():
 
     Answer:
     """
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
-    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    model = ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.3)
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     return chain
 
-def handle_yes_click(user_question):
-    st.write("Do you want me to search beyond the scope of contents provided. If yes, click yes")
-    if st.button("Yes"):
-        chain = get_conversational_chain()
-        response = chain({"input_documents": [], "question": user_question}, return_only_outputs=True)
-        st.write("Reply: ", response["output_text"])
-
 def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     new_db = FAISS.load_local("faiss_index", embeddings)
     docs = new_db.similarity_search(user_question)
-    st.write("Do you want me to search beyond the scope of contents provided.")
-    if st.button("Yes"):
-        handle_yes_click(user_question)
+    chain = get_conversational_chain()
+    response = chain({"input_documents":docs, "question": user_question}, return_only_outputs=True)
+    print(response)
+    st.write("Reply: ", response["output_text"])
 
 def main():
     st.set_page_config("Chat with PDF")
